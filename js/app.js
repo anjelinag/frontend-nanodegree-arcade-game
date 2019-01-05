e6956b4cd42bf999497a4fbd13d6dd0a1c32f2f3
Anjelina Gulla <anjelina.sertse@gmail.com>
2019-01-05 12:08:48 -0500

    have the enemy move on the x-axis

--- js/app.js
+++ js/app.js
@@ -47,6 +47,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -65,7 +66,7 @@ Enemy.prototype.render = function() {
 
 allEnemies = []; //there will be multiple enemies depending on the level of the game.
 
-var myEnemy = new Enemy(70, 0, 140);
+var myEnemy = new Enemy(200, 0, 140);
 var player = new Player(90, 200, 400);
 
 allEnemies.push(myEnemy);
